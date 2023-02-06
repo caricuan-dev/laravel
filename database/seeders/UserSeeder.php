6d59030b8f6c294fec311db2fc76f4293adb3626
caricuan-dev <caricuanyt23@gmail.com>
2023-02-07 02:07:27 +0700

    BIT Dev Laravel Starter

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,6 +23,15 @@ class UserSeeder extends Seeder
                 'no_hp' => '082237540250',
                 'role' => '1',
                 'status' => '1'
+            ],
+            [
+                'name' => 'Lamtaruli Sitohang',
+                'email' => 'quensya.uli@gmail.com',
+                'password' => bcrypt('123456'),
+                'email_verified_at' => '2023-01-06 13:45:10',
+                'no_hp' => '082237540250',
+                'role' => '1',
+                'status' => '1'
             ]
         ];
 
